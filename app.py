d66eaca4b8269afad6354aeafdb863a0de84bc1c
Alec Swift <aleceswift@gmail.com>
2023-10-18 16:44:05 -0700

    Write code for the post/get method for the index functionn

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/index.html")
+@app.route("/index.html", methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    error = None
+    if request.method == 'POST':
+        source = request.form['source']
+        destination = request.form['destination']
+        if valid_address(source) and valid_address(destination):
+            return safe_path(source, destination)
+        error = 'Invalid Address'
+
+    return render_template("index.html", error=error)
+
+def valid_address(address):
+    return True
+
+def safe_path(source, destination):
+    pass
