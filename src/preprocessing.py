76572d55e97a2d53d5e2005a82100f21ff8a8387
Alec Swift <aleceswift@gmail.com>
2023-11-13 12:20:50 -0800

    Debug the preprocessing file to more quickly write data to csv #6

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,9 +1,12 @@
 
+from csv import writer
 from random import randint
+import time
 
 import numpy as np
 import pandas as pd
 from fastai.tabular.all import *
+
 
 # First import pandas to help us work with the collision data excel file.
 
@@ -16,19 +19,26 @@ add_datepart(raw_collisions, 'INCDATE')
 
 # Here we read our collisions data file into the program as a DataFrame. We set the pandas options to display the max columns so pandas doesn't hide any of the columns when we display the DataFrame. The low memory, False key value pair also ensures the displayed DataFrame doesn't hide any data. Additionally, we add a column for our dependant variable, accident, whether or not there was an accident. Some important columns to look at for the machine learning model are location: ('X', 'Y'), time: 'INCDATE', weather: 'WEATHER', and light conditions: 'LIGHTCOND'. Finally, we'll use the fastai function add_datepart to split up our date column to change the data from categorical to numerical. 
 
-
-collisions = raw_collisions[[
+column_names = [
     'X', 'Y', 'SEVERITYCODE', 'WEATHER', 'LIGHTCOND',
     'INCDATEYear', 'INCDATEMonth', 'INCDATEWeek',
     'INCDATEDay', 'INCDATEDayofweek', 'INCDATEDayofyear'
-]].copy()
+]
+collisions = raw_collisions[column_names].copy()
+collisions.to_csv("/home/alec/Desktop/code/personal_projects/safe-path-finder/data/collisions.csv", index=False)
 
-new_collisions = pd.DataFrame(columns=collisions.columns)
+with open('/home/alec/Desktop/code/personal_projects/safe-path-finder/data/new_collisions.csv', 'w') as csv_file:
+    csv_writer = writer(csv_file)
+    csv_writer.writerow(column_names)
+
 num = 1
+start = time.perf_counter()
 
 while num <= 242131 * 3:
     if num % 10000 == 0:
-        print(num/(242131 * 3)*100, '%')
+        end = time.perf_counter()
+        print(num/(242131 * 3)*100, '%', end - start)
+        start = end
     rand_index_1 = randint(0, len(collisions) - 1)
     row = collisions.iloc[rand_index_1].copy(deep=True)
     rand_index_2 = randint(0, len(collisions) - 1)
@@ -47,8 +57,5 @@ while num <= 242131 * 3:
     ).any()
     if exists:
         continue
-    new_collisions = pd.concat([new_collisions, row.to_frame().T], ignore_index=True)
+    row.to_frame().T.to_csv("/home/alec/Desktop/code/personal_projects/safe-path-finder/data/new_collisions.csv", header=False, index=False, mode='a')
     num += 1
-
-new_collisions.to_csv("/home/alec/Desktop/code/personal_projects/safe-path-finder/data/new_collisions.csv")
-collisions.to_csv("/home/alec/Desktop/code/personal_projects/safe-path-finder/data/collisions.csv")
