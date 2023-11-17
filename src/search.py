923b1a55d49599db07c2d17b79992e7d255c85aa
Ubuntu <ubuntu@fast.book.course>
2023-11-17 01:24:55 +0000

    Export graph to igraph #7

--- src/search.py
+++ src/search.py
@@ -1,6 +1,7 @@
 from pyrosm import OSM, get_data
 
-data_seattle = get_data("Seattle", directory="/home/alec/Desktop/code/personal_projects/safe-path-finder/data")
+data_seattle = get_data("Seattle", directory="/home/ubuntu/safe-path-finder/data")
 osm_seattle = OSM(data_seattle)
 nodes, edges = osm_seattle.get_network(nodes=True, network_type="driving")
-nodes.head()
+graph = osm_seattle.to_graph(nodes, edges)
+#load this data to a pickle file for later
