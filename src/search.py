9fd8a56e3dfd2a6c3621cbbd64000012f499b22c
Alec Swift <aleceswift@gmail.com>
2023-11-19 12:43:19 -0800

    Add documentation for the search algorith

--- src/search.py
+++ src/search.py
@@ -2,7 +2,6 @@ import heapq
 import pickle
 
 from pyrosm import get_data, OSM
-from shapely import Point
 from haversine import haversine
 
 def build_graph(directory):
@@ -20,57 +19,10 @@ def build_graph(directory):
 
 # convert user input to a start/end in the graph by finding the closest point in the graph either using the vs.find function or another method as this won't work for every coordinate
 
-def a_star_search(start, end):
-    """
-    parameters
-    start: starting vertex in the graph
-    end: ending/target vertex in the graph
-    return: path through the graph
-    """
-    entry = 1
-    minheap = [(0, 0, start, 0)] # add first item
-    distances = {start: (0, None)} # also the visited set
-    end_lat, end_lon = end['lat'], end['lon']
-
-    # need to create node object with parents to find path
-    while minheap:
-        _, _, curr_node, curr_dist = heapq.heappop(minheap)
-
-        if curr_node == end:
-            break
-
-        for out_edge_idx in seattle_graph.incident(curr_node, mode="out"):
-            out_edge = seattle_graph.es[out_edge_idx]
-            new_dist = curr_dist + out_edge["length"]
-            neighbor = seattle_graph.vs.find(id=out_edge["v"])
-            dist_to_end = haversine((neighbor['lat'], neighbor['lon']), (end_lat, end_lon)) * 1000 # kilometers to meters
-
-
-            # need to add parent traversal
-            if neighbor not in distances or new_dist < distances[neighbor][0]:
-                distances[neighbor] = (new_dist, curr_node)
-            else: # visited
-                continue
-            
-            weight = 2 * (new_dist + dist_to_end)
-            heapq.heappush(minheap, (weight, entry, neighbor, new_dist))
-            entry += 1
-
-
-    path = []
-    curr = distances[end][1]
-    while curr is not None:
-        path.append(curr)
-        curr = distances[curr][1]
-
-    return path[::-1]
-
-
 if __name__ == "__main__":
     # build_graph("/home/ubuntu/safe-path-finder/src")
     with open("src/graph.pkl", "rb") as in_file:
         seattle_graph = pickle.load(in_file)
-    path = a_star_search(seattle_graph.vs[0], seattle_graph.vs[5])
     print(path)
     
     
