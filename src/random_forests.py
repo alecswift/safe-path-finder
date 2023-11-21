fac0e83b098722cc0c366e042f34e87a24c39a87
Alec Swift <aleceswift@gmail.com>
2023-11-20 20:23:51 -0800

    Comment out unecessary code

--- src/random_forests.py
+++ src/random_forests.py
@@ -1,7 +1,8 @@
 import joblib
 from fastai.tabular.all import *
+import osmnx
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
 
 def organize_data(collisions_file, new_collisions_file):
     """
@@ -29,22 +30,36 @@ def organize_data(collisions_file, new_collisions_file):
 
     return organized_data
 
-def random_forest(data, file_name):
+def random_forest_builder(data, file_name):
     """Saves the random forest generated from the data to the file name"""
     indep_vars, dep_vars = data.train.xs, data.train.y
 
-    random_forest = RandomForestRegressor(n_jobs=-1, n_estimators=50,
+    random_forest = RandomForestClassifier(n_jobs=-1, n_estimators=50,
         max_samples=200000, max_features=0.5,
         min_samples_leaf=20, oob_score=True)
     random_forest.fit(indep_vars, dep_vars)
 
     joblib.dump(random_forest, file_name)
 
-def predictor()
-
-if __name__ == "__main__":
-    data = organize_data(
-        "/home/alec/Desktop/code/personal_projects/safe-path-finder/data/collisions.csv",
-        "/home/alec/Desktop/code/personal_projects/safe-path-finder/data/new_collisions.csv"
+def predictor(location, weather, light_cond, day, week, month, year, week_day, year_day):
+    random_forest = joblib.load("/home/alec/Desktop/code/personal_projects/safe-path-finder/src/random_forest.joblib")
+    weather_names = ['na', 'Blowing Sand/Dirt', 'Blowing Snow', 'Clear', 'Fog/Smog/Smoke', 'Other', 'Overcast', 'Partly Cloudy', 'Raining', 'Severe Crosswind', 'Sleet/Hail/Freezing Rain', 'Snowing', 'Unknown']
+    light_cond_names = ['na', 'Dark - No Street Lights', 'Dark - Street Lights Off', 'Dark - Street Lights On', 'Dark - Unknown Lighting', 'Dawn', 'Daylight', 'Dusk', 'Other', 'Unknown']
+    light_cond_dict = {light_cond: idx for idx, light_cond in enumerate(light_cond_names)}
+    weather_dict = {name: idx for idx, name in enumerate(weather_names)}
+    x, y = osmnx.geocode(location)
+    indep_vars = pd.DataFrame(
+        columns=['WEATHER', 'LIGHTCOND', 'X_na', 'Y_na', 'X', 'Y', 'INCDATEYear',
+        'INCDATEMonth', 'INCDATEWeek', 'INCDATEDay', 'INCDATEDayofweek',
+        'INCDATEDayofyear']
     )
-    random_forest(data, "/home/alec/Desktop/code/personal_projects/safe-path-finder/src/random_forest.joblib")
+    indep_vars.loc[len(indep_vars)] = weather_dict[weather], light_cond_dict[light_cond], 1, 1, x, y, year, month, week, day, week_day, year_day
+    return random_forest.predict(indep_vars)
+
+# if __name__ == "__main__":
+    # data = organize_data(
+    #    "/home/alec/Desktop/code/personal_projects/safe-path-finder/data/collisions.csv",
+    #   "/home/alec/Desktop/code/personal_projects/safe-path-finder/data/new_collisions.csv"
+    #)
+    # random_forest_builder(data, "/home/alec/Desktop/code/personal_projects/safe-path-finder/src/random_forest.joblib")
+    # print(predictor("8814, 28th Avenue Northwest, North Beach, Seattle, King County, Washington, 98117, United States", "Snowing", "Dusk", 1, 1, 1, 2024, 1, 1))
