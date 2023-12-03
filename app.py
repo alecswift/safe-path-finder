d06413e2625aa7f1631601d7d528f1efc6382034
Alec Swift <aleceswift@gmail.com>
2023-12-02 21:51:40 -0800

    add database functionality

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import sqlite3
 import sys
 sys.path.insert(0, './src')
 from search import shortest_path, valid_address, get_directions
@@ -22,6 +23,11 @@ def addresses():
         if valid_address(source) and valid_address(destination):
             routes = shortest_path(source, destination)
             directions = get_directions(routes)
+            with sqlite3.connect('database.sqlite') as con:
+                cursor = con.cursor()
+                cursor.execute("INSERT INTO database VALUES (?, ?, ?)", (source, destination, directions))
+
+                con.commit()
             return render_template("directions.html", directions=directions)
         error = 'Invalid Address'
 
