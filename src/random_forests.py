9f6c5dfb03106eabb315586a7c56e3850ca66501
Alec Swift <aleceswift@gmail.com>
2023-12-03 09:45:52 -0800

    Change the random forest classifier to the random forest regressor

--- src/random_forests.py
+++ src/random_forests.py
@@ -2,7 +2,7 @@ import joblib
 from fastai.tabular.all import *
 import osmnx
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 
 def organize_data(collisions_file, new_collisions_file):
     """
@@ -34,7 +34,7 @@ def random_forest_builder(data, file_name):
     """Saves the random forest generated from the data to the file name"""
     indep_vars, dep_vars = data.train.xs, data.train.y
 
-    random_forest = RandomForestClassifier(n_jobs=-1, n_estimators=50,
+    random_forest = RandomForestRegressor(n_jobs=-1, n_estimators=50,
         max_samples=200000, max_features=0.5,
         min_samples_leaf=20, oob_score=True)
     random_forest.fit(indep_vars, dep_vars)
